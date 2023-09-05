3b435c389b1681704b3305622d8aedbdc4ed75c0
kokurate <fredriksanger@gmail.com>
2023-09-05 15:06:00 +0800

    Integrate Tabler Admin Template

---
+++ resources/views/back/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('back.layouts.pages-layout')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Home')
+@section('content')
+
+content here
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('author')->name('author')->group(function(){
+Route::prefix('author')->name('author.')->group(function(){
 
     Route::middleware(['guest:web'])->group(function()
     {
