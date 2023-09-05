a67992d98c2d4430df8f1ec7275d2951f059b4ba
kokurate <fredriksanger@gmail.com>
2023-09-05 18:31:05 +0800

    Login with Email Or Username in one field

--- resources/views/livewire/author-login-form.blade.php
+++ resources/views/livewire/author-login-form.blade.php
@@ -8,9 +8,9 @@
 
     <form wire:submit.prevent="LoginHandler()" method="get" autocomplete="off" novalidate>
         <div class="mb-3">
-          <label class="form-label">Email address</label>
-          <input type="email" class="form-control" placeholder="your@email.com" autocomplete="off" wire:model="email">
-          @error('email')
+          <label class="form-label">Email or Username</label>
+          <input type="text" class="form-control" placeholder="Enter email or username" autocomplete="off" wire:model="login_id">
+          @error('login_id')
             <span class="text-danger">{{ $message }}</span>
           @enderror
         </div>
