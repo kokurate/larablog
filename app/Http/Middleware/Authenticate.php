848d7f57c33f6761e28b05c2ce57d9b3cbe5230a
kokurate <fredriksanger@gmail.com>
2023-09-06 21:17:05 +0800

    Implementing the 'return URL' functionality

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\URL;
 
 class Authenticate extends Middleware
 {
@@ -19,7 +20,7 @@ class Authenticate extends Middleware
 
             if($request->routeIs('author.*')){
                 session()->flash('fail', 'You Must Sign First');
-                return route('author.login');
+                return route('author.login',['fail' => true, 'returnUrl' => URL::current()]);
             }
 
         }
