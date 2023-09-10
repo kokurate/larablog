30f6cb7c46ad0c0990f6d40215401a0979c10e92
kokurate <fredriksanger@gmail.com>
2023-09-10 15:46:06 +0800

    Displaying the Latest Posts and the 6 Most Recent Posts with functions for date formatting, word count, and read duration integrated into the custom helper

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -14,9 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('front.pages.example');
-});
+// Route::get('/', function () {
+//     return view('front.pages.example');
+// });
 
+Route::view('/','front.pages.home')->name('home');
+
+Route::get('/article/{any}',[BlogController::class,'readPost'])->name('read_post');
+Route::get('/category/{any}',[BlogController::class,'categoryPosts'])->name('category_posts');
+Route::get('/posts/tag/{any}',[BlogController::class,'tagPosts'])->name('tag_posts');
+Route::get('/search',[BlogController::class,'searchBlog'])->name('search_posts');
 
 
